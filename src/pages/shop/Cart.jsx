49443a637084d90c9811f077e6813ddbae3691a6
rycardodias <rycardo.dias@hotmail.com>
2022-01-19 01:08:30 +0000

    .

--- src/pages/shop/Cart.jsx
+++ src/pages/shop/Cart.jsx
@@ -13,6 +13,8 @@ const Cart = () => {
   useTitle(t("Cart"));
 
   const [data, setdata] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
 
   async function fetchProducts() {
     const res = await cartsRequests.getCartByUserWithProduct()
@@ -20,6 +22,7 @@ const Cart = () => {
     if (res.error) return
     if (res.data.error) return
     setdata(res.data.data)
+    setIsLoading(false)
   }
 
   useEffect(() => {
@@ -27,15 +30,17 @@ const Cart = () => {
   }, [])
 
   function handleRemoveItem(id) {
+    setIsLoading(true)
     const remainingItems = data.filter(item => item.id !== id)
     setdata(remainingItems)
+    setIsLoading(false)
   }
 
 
   return <Box pt={2} pb={4}>
     <Grid container spacing={3}>
       <Grid item md={7} sm={7} xs={12}>
-        {data.map((item, index) => <CartListItem key={index} item={item} removeItemList={handleRemoveItem} />)}
+        {!isLoading && data.map((item, index) => <CartListItem key={index} item={item} removeItemList={handleRemoveItem} />)}
       </Grid>
       <Grid item md={5} sm={5} xs={12}>
         <OrderSummery data={data} />
