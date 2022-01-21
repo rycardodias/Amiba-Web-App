147d904d1c749cb2ef6f4812f2e85f35066b717b
rycardodias <rycardo.dias@hotmail.com>
2022-01-21 13:44:08 +0000

    .

--- src/pages/shop/Cart.jsx
+++ src/pages/shop/Cart.jsx
@@ -60,10 +60,10 @@ const Cart = () => {
     <Grid container spacing={3}>
       <Grid item md={7} sm={7} xs={12}>
         {/* {!isLoading && console.log('data', data)} */}
-        {!isLoading && data.map((item, index) => <CartListItem key={index} item={item} btnClick={handleBtnClick} removeItemList={handleRemoveItem} refreshQuantity={handleRefreshQuantity} />)}
+        {!isLoading && data.map((item, index) => <CartListItem key={index} item={item}  removeItemList={handleRemoveItem} refreshQuantity={handleRefreshQuantity} />)}
       </Grid>
       <Grid item md={5} sm={5} xs={12}>
-        <OrderSummery data={data} />
+        <OrderSummery data={data} btnClick={handleBtnClick} />
       </Grid>
     </Grid>
   </Box>;
